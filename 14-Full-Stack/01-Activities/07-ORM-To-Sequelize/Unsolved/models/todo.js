dbcd1d055580d1e439e6ecad44722cfac157b57f
C.T. Bell <charlestbell@gmail.com>
2021-02-13 13:44:06 -0500

    Adding files from class

--- 14-Full-Stack/01-Activities/07-ORM-To-Sequelize/Unsolved/models/todo.js
+++ 14-Full-Stack/01-Activities/07-ORM-To-Sequelize/Unsolved/models/todo.js
@@ -1,3 +1,23 @@
-const sequalize = require("sequelize");
+const { Sequelize, DataTypes } = require("sequelize");
+const sequelize = new Sequelize("sqlite::memory:");
 
-const todo = {};
+const User = sequelize.define(
+  "User",
+  {
+    // Model attributes are defined here
+    text: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    complte: {
+      type: DataTypes.BOOLEAN,
+      // allowNull defaults to true
+    },
+  },
+  {
+    // Other model options go here
+  }
+);
+
+// `sequelize.define` also returns the model
+console.log(User === sequelize.models.User); // true
